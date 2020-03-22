bc4e8fcd5f0e041cf68fc91dda7bbe5985616ada
Artem Gotmanov <artem.gotmanov@gmail.com>
2020-03-22 18:26:23 +0300

    Реализация бургер-меню

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("hamburger-menu-link").onclick = function() {
+	open()
+ };
+ 
+ function open() {
+	document.getElementById("hamburger-menu").classList.toggle("hamburger-menu--activ");
+ }
+
+ document.getElementById("hamburger-menu__img-link").onclick = function() {
+	open()
+ };
