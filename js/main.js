2cd5271001982ccc4e75fcf72bbeb06fe44ae7bd
Artem Gotmanov <artem.gotmanov@gmail.com>
2020-03-23 20:14:15 +0300

    Реализация раскрытия меню в секции Команда и Меню

--- js/main.js
+++ js/main.js
@@ -1,11 +1,54 @@
-document.getElementById("hamburger-menu-link").onclick = function() {
-	open()
- };
- 
- function open() {
-	document.getElementById("hamburger-menu").classList.toggle("hamburger-menu--activ");
- }
+// реализация бургер-меню
 
- document.getElementById("hamburger-menu__img-link").onclick = function() {
+document.getElementById("hamburger-menu-link").onclick = function() {
+	openBurgerMenu()
+};
+ 
+function openBurgerMenu() {
+	document.getElementById("hamburger-menu").classList.toggle("hamburger-menu--activ");
+}
+
+document.getElementById("hamburger-menu__img-link").onclick = function() {
 	open()
- };
+};
+
+// реализация списка "Команда"
+
+document.getElementById("team__item--1").onclick = function() {
+	openTeam("team__item--1");
+
+};
+
+document.getElementById("team__item--2").onclick = function() {
+	openTeam("team__item--2")
+};
+
+document.getElementById("team__item--3").onclick = function() {
+	openTeam("team__item--3")
+};
+
+document.getElementById("team__item--4").onclick = function() {
+	openTeam("team__item--4")
+};
+
+function openTeam(team) {
+	document.getElementById(team).classList.toggle("team__item--activ");
+}
+
+// реализация списка "Меню"
+
+document.getElementById("carte__item--1").onclick = function() {
+	openCarte("carte__item--1")
+};
+
+document.getElementById("carte__item--2").onclick = function() {
+	openCarte("carte__item--2")
+};
+
+document.getElementById("carte__item--3").onclick = function() {
+	openCarte("carte__item--3")
+};
+
+function openCarte(carte) {
+	document.getElementById(carte).classList.toggle("carte__item--activ");
+}
