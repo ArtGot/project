e9fe0537a1bc9aee9173cc1d95c3a62ce9a658e4
Artem Gotmanov <artem.gotmanov@gmail.com>
2020-04-04 19:57:21 +0300

    Подключение слайдера на jq

--- js/main.js
+++ js/main.js
@@ -60,26 +60,45 @@ function toogleClassCarte(ev) {
 
 // реализация Слайдера
 
-const left = document.querySelector(".scroll__left-link");
-const right = document.querySelector(".scroll__right-link");
-const items = document.querySelector(".burger__items");
+// const left = document.querySelector(".scroll__left-link");
+// const right = document.querySelector(".scroll__right-link");
+// const items = document.querySelector(".burger__items");
 
-right.addEventListener("click", function (e) {
-	loop("right", e);
-});
+// right.addEventListener("click", function (e) {
+// 	loop("right", e);
+// });
 
-left.addEventListener("click", function (e) {
-	loop("left", e);
-});
+// left.addEventListener("click", function (e) {
+// 	loop("left", e);
+// });
 
-function loop(direction, e) {
-	e.preventDefault();
-	if (direction === "right") {
-		items.appendChild(items.firstElementChild);
-	} else {
-		items.insertBefore(items.lastElementChild, items.firstElementChild);
-	}
-}
+// function loop(direction, e) {
+// 	e.preventDefault();
+// 	if (direction === "right") {
+// 		items.appendChild(items.firstElementChild);
+// 	} else {
+// 		items.insertBefore(items.lastElementChild, items.firstElementChild);
+// 	}
+// }
+
+$(document).ready(function(){
+
+	let slider = $('.slider').bxSlider();
+
+	$('.scroll__left-link').on('click',function(e){
+		e.preventDefault();
+
+		slider.goToPrevSlide();
+	});
+
+	$('.scroll__right-link').on('click',function(e){
+		e.preventDefault();
+
+		slider.goToNextSlide();
+	});
+
+ });
+
 
 // работа с формами
 
