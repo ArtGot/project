d9047be0d545455b47bff570cbf13f8d1fab1e0a
Artem Gotmanov <artem.gotmanov@gmail.com>
2020-04-05 22:20:59 +0300

    Подключение карты

--- js/main.js
+++ js/main.js
@@ -290,3 +290,41 @@ function onYouTubeIframeAPIReady() {
 };
 
 eventsInit();
+
+// Подключение карты
+
+let myMap;
+
+const init =() => {
+	myMap = new ymaps.Map("map", {
+		center: [55.76, 37.64],
+		zoom: 15,
+		controls: []
+	});
+
+const coords = [
+	[55.762308, 37.639010],
+	[55.760495, 37.646399],
+	[55.765273, 37.639293],
+	[55.759561, 37.638925]
+];
+
+const myCollection = new ymaps.GeoObjectCollection({}, {
+	draggable: false,
+	iconLayout: 'default#image',
+	iconImageHref: '/img/icon/map-marker.png',
+	iconImageSize: [46, 57],
+	iconImageOffset: [-35, -52]
+});
+
+coords.forEach(coord => {
+	myCollection.add(new ymaps.Placemark(coord));
+});
+
+myMap.geoObjects.add(myCollection);
+
+myMap.behaviors.disable('scrollZoom');
+
+};
+
+ymaps.ready(init);
